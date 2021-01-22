d7a65d5bf6a0064dc21bddae0a2c4eb0de87f05f
Diego Ignacio Nuñez Hernandez <diegodog2312sam@gmail.com>
2021-01-22 16:56:32 -0600

    holi

---
+++ FlappyBird/src/flappybird/Main.java
@@ -0,0 +1,12 @@
+package flappybird;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Holi");
+    }
+    
+}
