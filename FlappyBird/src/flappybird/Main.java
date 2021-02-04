7ab4b511c05e59cf2e0491c29f3889cda3b6a529
Diego Ignacio Nuñez Hernandez <diegodog2312sam@gmail.com>
2021-02-04 13:39:59 -0600

    ok

--- FlappyBird/src/flappybird/Main.java
+++ FlappyBird/src/flappybird/Main.java
@@ -6,13 +6,13 @@ package flappybird;
  */
 public class Main {
 
-    //Reemplace la URL por la ruta en donde se encuentre la carpeta de multimedios
+    // Reemplace la URL por la ruta en donde se encuentre la carpeta de multimedios
     final public static String URL = "/Users/diegoignacionunezhernandez/OneDrive - Escuela Nacional Colegio de Ciencias y Humanidades, UNAM/POO_/Proyecto 1/Proyecto-Flappy-Bird/FlappyBird/src/multimedia/";
     /**
      * Método principal, en él se instanciarán los personajes del juego y el controlador.Además, se iniciará el juego.
      * @param args the command line arguments
      * @throws java.lang.InterruptedException Excepción en caso de no poder iniciar el juego.
-     */
+     */ 
     public static void main(String[] args) throws InterruptedException {                
         Bird bird = new Bird();
         Obstaculo arbol1 = new Obstaculo(0);
